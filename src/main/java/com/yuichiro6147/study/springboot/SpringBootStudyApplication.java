f05643c23178375335b52d399f1ddbb75cba402e
Yuichiro Takeda <y-takeda.6147@i.softbank.jp>
2018-01-01 00:36:10 +0900

    initial commit of SpringBoot

---
+++ src/main/java/com/yuichiro6147/study/springboot/SpringBootStudyApplication.java
@@ -0,0 +1,12 @@
+package com.yuichiro6147.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+}
